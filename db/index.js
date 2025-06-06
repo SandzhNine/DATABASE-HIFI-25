05a880a9bc92222626d9d4e2155d3198fa6906bc
Sandy Fauzi <132027986+SandzhNine@users.noreply.github.com>
2025-06-06 20:36:16 +0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const { urlDb } = require('../config');
+const { urlDb } = require('../config');  // Pastikan ada config urlDb yang benar
 
-mongoose.connect(urlDb, {
-   useUnifiedTopology: true
-})
+// Fungsi koneksi singleton untuk reuse connection
+const connectDB = async () => {
+  if (mongoose.connection.readyState === 1) {  // 1 = connected
+    return mongoose.connection;
+  }
+  return mongoose.connect(urlDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+};
 
-const db = mongoose.connection;
-
-module.exports = db;
+module.exports = connectDB;
