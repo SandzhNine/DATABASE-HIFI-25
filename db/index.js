1fbe08fa794087d33425e1e692f3a1a3da62822e
Sandy Fauzi <132027986+SandzhNine@users.noreply.github.com>
2025-06-06 20:42:14 +0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
-const { urlDb } = require('../config');  
-
+const { urlDb } = require('../config');
 
 const connectDB = async () => {
-  if (mongoose.connection.readyState === 1) {  
+  if (mongoose.connection.readyState === 1) {
     return mongoose.connection;
   }
-  return mongoose.connect(urlDb, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+  try {
+    const conn = await mongoose.connect(urlDb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error;  
+  }
 };
 
 module.exports = connectDB;
